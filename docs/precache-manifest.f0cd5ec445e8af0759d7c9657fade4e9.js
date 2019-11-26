f9eeb54078e332a35e18552430897326896a56eb
tomdurrent <tomdurrent@sero.vip>
2019-11-26 18:58:16 +0800

    update build path to docs

---
+++ docs/precache-manifest.f0cd5ec445e8af0759d7c9657fade4e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f1fbe05d279dd55e099c59a6562208",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84a069c4595c75f0b14a",
+    "url": "./static/css/2.6c3b5ba6.chunk.css"
+  },
+  {
+    "revision": "172f12f26471430b37ff",
+    "url": "./static/css/main.10a5d573.chunk.css"
+  },
+  {
+    "revision": "0666b0f60fb7a2dbf2e45984babe8c02",
+    "url": "./static/js/0.0d42a441.chunk.worker.js"
+  },
+  {
+    "revision": "4a23b95c53fcc2176f9e2a422f627156",
+    "url": "./static/js/1.7efda693.chunk.worker.js"
+  },
+  {
+    "revision": "84a069c4595c75f0b14a",
+    "url": "./static/js/2.6976fbe0.chunk.js"
+  },
+  {
+    "revision": "172f12f26471430b37ff",
+    "url": "./static/js/main.799f05aa.chunk.js"
+  },
+  {
+    "revision": "f8219e59cd933c2cdcd1",
+    "url": "./static/js/runtime~main.ca7cb530.js"
+  },
+  {
+    "revision": "2f0a4ff7c7262b76075345f229f7d61f",
+    "url": "./static/media/logo.2f0a4ff7.png"
+  },
+  {
+    "revision": "ef3f451460d037e49c8c3a5d31c31686",
+    "url": "./static/media/sero.ef3f4514.png"
+  }
+]);

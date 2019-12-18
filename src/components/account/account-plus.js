cda51435175a812b2432a1fb06b3c397e17036eb
tomdurrent <tomdurrent@sero.vip>
2019-12-18 13:24:14 +0800

    dev

---
+++ src/components/account/account-plus.js
@@ -0,0 +1,24 @@
+class AccountPlus {
+
+    genWord(){
+
+    }
+
+    getSK(password){
+
+    }
+
+    PreCreate(name, password, hint, word){
+
+    }
+
+    CopyTempKeystore() {
+
+    }
+}
+
+const accountPlus = new AccountPlus();
+
+export {
+    accountPlus
+}

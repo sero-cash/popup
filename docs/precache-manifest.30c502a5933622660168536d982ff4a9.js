cef41e097a5949c1db7edccced2b8bea5935e8e7
tomdurrent <tomdurrent@sero.vip>
2019-12-30 18:55:42 +0800

    upgrade version v1.1.2

--- docs/precache-manifest.30c502a5933622660168536d982ff4a9.js
+++ docs/precache-manifest.30c502a5933622660168536d982ff4a9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5cc291ad59e8291ca6fea4c903bf243",
+    "revision": "fee47e326d59a8782e83995c6ba2557c",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.2765dbdc.chunk.css"
   },
   {
-    "revision": "126dcba927f1c0333370",
+    "revision": "9df841d65558690e1d7d",
     "url": "./static/css/main.1f0f39f4.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3b4b63fc.chunk.js"
   },
   {
-    "revision": "126dcba927f1c0333370",
-    "url": "./static/js/main.a6164f9c.chunk.js"
+    "revision": "9df841d65558690e1d7d",
+    "url": "./static/js/main.4ddc9263.chunk.js"
   },
   {
     "revision": "f8219e59cd933c2cdcd1",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.30f418fbeeea51555611e9ed5bc71f91.js"
+  "./precache-manifest.30c502a5933622660168536d982ff4a9.js"
 );
 
 self.addEventListener('message', (event) => {

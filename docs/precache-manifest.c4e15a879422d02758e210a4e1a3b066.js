425c5b9a8d2fc083c673332cafd4b8930c410cb6
tomdurrent <tomdurrent@sero.vip>
2020-05-08 15:45:45 +0800

    remove unused testcode

---
+++ docs/precache-manifest.c4e15a879422d02758e210a4e1a3b066.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0111889afbbb6a623e75bef041f5e943",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34b30f913ce21eee57d2",
+    "url": "./static/css/2.067177ae.chunk.css"
+  },
+  {
+    "revision": "9020d9b9b5a28548cfc7",
+    "url": "./static/css/main.71bfb037.chunk.css"
+  },
+  {
+    "revision": "d4ca405310b5fd1ed0e93d8a113abb7b",
+    "url": "./static/js/0.10d23c1b.chunk.worker.js"
+  },
+  {
+    "revision": "258a190064289be6c92bd808081521c2",
+    "url": "./static/js/1.2b78372a.chunk.worker.js"
+  },
+  {
+    "revision": "34b30f913ce21eee57d2",
+    "url": "./static/js/2.4e84ff77.chunk.js"
+  },
+  {
+    "revision": "9020d9b9b5a28548cfc7",
+    "url": "./static/js/main.5c31ef8e.chunk.js"
+  },
+  {
+    "revision": "f8219e59cd933c2cdcd1",
+    "url": "./static/js/runtime~main.ca7cb530.js"
+  },
+  {
+    "revision": "2f0a4ff7c7262b76075345f229f7d61f",
+    "url": "./static/media/logo.2f0a4ff7.png"
+  },
+  {
+    "revision": "ef3f451460d037e49c8c3a5d31c31686",
+    "url": "./static/media/sero.ef3f4514.png"
+  }
+]);
